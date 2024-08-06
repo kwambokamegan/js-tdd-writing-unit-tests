8f958c60d98337833a353db7d64d539a1cc93569
Megan <megan.nyakina@student.moringaschool.com>
2024-08-06 23:34:29 +0300

    js writing unit tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,24 @@
 // Your tests here
+import { currentAgeForBirthYear } from "../utils";
+
+describe("currentAgeForBirthYear", () => {
+  it("returns the age of a person based on the year of birth", () => {
+    const currentYear = new Date().getFullYear();
+    const birthYear = 1984;
+    const answer = currentYear - birthYear;
+
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+    expect(ageOfPerson).toEqual(answer);
+  });
+
+  it("returns the current year for a person born in year 0", () => {
+    const birthYear = 0;
+    const currentYear = new Date().getFullYear();
+    const answer = currentYear - birthYear;
+
+    const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+    expect(ageOfPerson).toEqual(answer);
+  });
+});
